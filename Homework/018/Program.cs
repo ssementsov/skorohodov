9c2fdba71555def685f7076e4279046305be3ef8
XsansanichX <xsansanichx@gmail.com>
2022-01-02 15:20:19 +0300

    task 13, 18
    Hello, Sergei!
    Please, check my homework(Task 13, 18)

---
+++ Homework/018/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your number");
+            string usersNumber = Console.ReadLine();
+
+            usersNumber = usersNumber.Replace("-", "");
+            usersNumber = usersNumber.Replace(".", "");
+            usersNumber = usersNumber.Replace(",", "");
+
+            char[] usersNumberArray = usersNumber.ToCharArray();
+            Array.Reverse(usersNumberArray);
+            string reverseUsersNumber = new string(usersNumberArray);
+
+            bool isUsersNumberPalindrom = (usersNumber == reverseUsersNumber);
+
+            if (isUsersNumberPalindrom)
+            {
+                Console.WriteLine("Your number is palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Your number is not palindrom");
+            }
+            Console.ReadKey();
+        }
+    }
+}
