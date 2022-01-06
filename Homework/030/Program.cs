c875de9222674637e540a8efa7905883d3c3c813
XsansanichX <xsansanichx@gmail.com>
2022-01-06 23:51:40 +0300

    Task 26, 30
    Sergey, check it, please!

---
+++ Homework/030/Program.cs
@@ -0,0 +1,29 @@
+﻿// Task 30
+Console.Write("введите первое число:            ");
+double usersFirstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("введите второе число:            ");
+double usersSecondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("введите шаг выведения значений:  ");
+double usersStep = Convert.ToDouble(Console.ReadLine());
+
+if (usersFirstNumber > usersSecondNumber & usersStep < 0)
+{
+    for (double i = usersFirstNumber; i >= usersSecondNumber; i+= usersStep)
+    {
+        Console.WriteLine(($"{i:f1}"));   
+    }
+}
+else if (usersFirstNumber < usersSecondNumber & usersStep > 0)
+{
+    for (double i = usersFirstNumber; i <= usersSecondNumber; i += usersStep)
+    {
+        Console.WriteLine($"{i:f1}");
+    }
+}
+else
+{
+    Console.WriteLine("введите корректные значения!");
+}
+Console.ReadKey();  
