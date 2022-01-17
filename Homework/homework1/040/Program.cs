b9533d708ac53c56f604a44172d2b0cbf6f39d72
XsansanichX <xsansanichx@gmail.com>
2022-01-18 00:00:55 +0300

    Task40

---
+++ Homework/homework1/040/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task040
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter your string: ");
+            string usersString = Console.ReadLine();
+
+            ToLowerCase(usersString);
+            Console.ReadKey();
+        }
+
+        public static void ToLowerCase(string usersString)
+        {
+            char[] usersCharFromStringArray = usersString.ToCharArray();
+            int[] usersIntFromStringArray = new int[usersCharFromStringArray.Length];
+            for (int i = 0; i < usersCharFromStringArray.Length; i++)
+            {
+                usersIntFromStringArray[i] = usersCharFromStringArray[i];
+            }
+
+            for (int j = 0; j < usersIntFromStringArray.Length; j++)
+            {
+                if (usersIntFromStringArray[j] < 97)
+                {
+                    usersIntFromStringArray[j] = usersIntFromStringArray[j] + 32;
+                }
+            }
+
+            Console.WriteLine("LowerCase String: ");
+            foreach (var item in usersIntFromStringArray)
+            {
+                Console.Write((char)item);
+            }
+        }
+    }
+}
