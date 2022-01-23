91ef7adee4458f7061d2d4449d66c54f79fc8d60
XsansanichX <xsansanichx@gmail.com>
2022-01-23 22:11:41 +0300

    Task 43, 44, 53

---
+++ Homework/homework1/043/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your text: ");
+            string usersString = Console.ReadLine();
+
+            Console.Write("Enter chars to remove from text: ");
+            string charsToRemove = Console.ReadLine();
+            
+            var result = ReverseString(RemoveChars(usersString, charsToRemove));
+            Console.WriteLine($"Your text after remove chars and reverse: {result}");
+
+            Console.ReadKey();
+        }
+        public static string RemoveChars(string usersString, string charsToRemove)
+        {
+            char[] arrayCharsToRemove = charsToRemove.ToCharArray();
+
+            foreach (char c in arrayCharsToRemove)
+            {
+                usersString = usersString.Replace(c.ToString(), "");
+            }
+
+                return usersString;
+        }
+        public static string ReverseString(string usersString)
+        {
+            var result = string.Empty;
+
+            for (int i = usersString.Length - 1; i >= 0; i--)
+            {
+                result += usersString[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Homework/homework1/053/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _053
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int leastCommonDivisor = LeastCommonDivisor(firstNumber, secondNumber);
+            Console.WriteLine($"Least common divisor of {firstNumber} and {secondNumber} is {leastCommonDivisor}");
+            Console.ReadKey();
+        }
+        static int LeastCommonDivisor (int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+            else
+            {
+                int min = Math.Min(a, b);
+                int max = Math.Max(a, b);
+                return LeastCommonDivisor(max-min, min);
+            }
+        }
+    }
+}
