ec740feccc023e5bd7dad0703df1ec6068078752
XsansanichX <xsansanichx@gmail.com>
2022-01-29 21:29:59 +0300

    Task 106, 107

---
+++ Homework/106/MyBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106
+{
+    public class MyBook : Book
+    {
+        public int BookPrice { get; set; }
+
+        public MyBook()
+        {
+            BookPrice = 35;
+        }
+
+        public MyBook(int BookPrice)
+        {
+            this.BookPrice = BookPrice;
+        }
+
+        public MyBook(string BooksTitle, string BookAuthor, int BookPrice)
+           : base(BooksTitle, BookAuthor)
+        {
+            this.BookPrice = BookPrice;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Price of \"{BooksTitle}\" is {BookPrice} USD.");
+        }
+    }
+}
---
+++ Homework/107/MyITBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106
+{
+    public class MyITBook : MyBook
+    {
+        public string Theme { get; set; }
+
+        public MyITBook()
+        {
+            Theme = "C# coding";
+        }
+
+        public MyITBook(int BookPrice, string Theme)
+        {
+            this.BookPrice = BookPrice;
+            this.Theme = Theme;
+        }
+
+        public MyITBook(string BooksTitle, string BookAuthor, int BookPrice, string Theme)
+           : base(BooksTitle, BookAuthor, BookPrice)
+        {
+            this.Theme = Theme;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Book \"{BooksTitle}\" by {BookAuthor} is to study {Theme}."); ;
+        }
+    }
+}
