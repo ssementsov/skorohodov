0782b36b8ba1498ba4c126d42fcda39123c297eb
XsansanichX <xsansanichx@gmail.com>
2022-02-07 22:08:02 +0300

    Task "Interface-7"

---
+++ Homework/OOP/Interfaces/7/Models/ConcreteClass.cs
@@ -0,0 +1,18 @@
+﻿using _7.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Models
+{
+    public class ConcreteClass: IInterface2, IInterface1
+    {
+        void IInterface1.Method() => Console.WriteLine("Method - implementation IInterface 1");
+
+        void IInterface2.Method() => Console.WriteLine("Method - implementation IInterface 2");
+
+        public void Method() => Console.WriteLine("Method - implementation IInterface (1-2)");
+    }
+}
